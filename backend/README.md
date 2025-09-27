8aee6172091f50b111464d8eacb6e99b1a578d06
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2025-09-27 17:48:42 -0400

    WIP: Emergency Room Agent

---
+++ backend/README.md
@@ -0,0 +1,16 @@
+```bash
+
+# Create
+python -m venv .venv
+# Activate (each new terminal)
+# macOS/Linux:
+source .venv/bin/activate
+
+# Windows CMD:
+.venv\Scripts\activate.bat
+
+# Windows PowerShell:
+.venv\Scripts\Activate.ps1
+
+pip install google-adk
+```
---
+++ backend/emergency_room_agent/agent.py
@@ -0,0 +1,36 @@
+from google.adk.agents import Agent
+from .sub_agents.doctor_agent.agent import doctor_agent
+from .sub_agents.nurse_agent.agent import nurse_agent
+
+
+
+MODEL_GEMINI_2_0_FLASH = "gemini-2.0-flash"
+
+root_agent = Agent(
+        name="emergency_room_agent",
+        model=MODEL_GEMINI_2_0_FLASH,
+        description="EMERGENCY ROOM AGENT who handles emergency room consultations and supervises trainee handovers.",
+        instruction="""
+            You are the primary EMERGENCY ROOM agent for the New Doctor Emergency Room Training.
+            Your role is to be the main facilitator of the training by allowing the user to communicate
+            to the appropriate specialized agent. As the levels of the state of the training change, a different
+            specialized agent may be needed to assist the user.
+
+            You should not communicate with the user, only the subagents will communicate with the user.
+            **Core Capabilities:**
+
+            You have access to the following specialized agents:
+
+            1. Doctor Agent
+            - Direct to doctor for a serious medical complication
+
+            2. Nurse Agent
+            - Direct to nurse for less serious medical issues or general health questions
+
+
+            Always maintain a helpful and professional tone. If you're unsure which agent to delegate to,
+            ask clarifying questions to better understand the user's needs.
+            """,
+            sub_agents=[doctor_agent, nurse_agent],
+            tools=[],
+)
